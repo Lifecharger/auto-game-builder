4e06939363e0b5b4af1f8ad8aeba7ead15a5d362
caca_ <koplayer17@gmail.com>
2026-08-05 01:31:27 +0300

    Release v1.0.145+146 to internal

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (hasKeystore) {
 
 android {
     namespace = "com.lifecharger.appmanager"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = maxOf(flutter.compileSdkVersion, 36)
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -43,7 +43,7 @@ android {
     defaultConfig {
         applicationId = "com.lifecharger.appmanager"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = maxOf(flutter.targetSdkVersion, 36)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
